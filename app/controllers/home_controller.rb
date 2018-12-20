61dc8c8ceeb62254fca50b82d5d78657fb5a1315
Erick Gurian <erick_gurian@hotmail.com>
2018-12-20 14:53:53 -0200

    Semana 2, tarefa 6: Administrador vê transações do dia - Completa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
     if params[:currency]
       @transactions = Transaction.where(currency: params[:currency])
     else
-    @transactions = Transaction.all
-    end 
+      @transactions = Transaction.all.where(created_at: Date.current.beginning_of_day..Date.current.end_of_day)
+    end
   end
 
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,9 @@
 class TransactionsController < ApplicationController
 
+  def index
+    @transactions = Transaction.all
+  end
+
   def new
     return create if transaction_params
     @transaction = Transaction.new
